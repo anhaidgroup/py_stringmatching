9ef61bff0158e42d0110fc99012c4567d0c693af
srujithpoondla <spoondla@wisc.edu>
2017-06-09 19:20:57 -0500

    Ignoring non unicode chars to overcome the unicodeDecodeError: Added 'ignore' in conver_to_unicode function

--- py_stringmatching/utils.py
+++ py_stringmatching/utils.py
@@ -85,7 +85,7 @@ def convert_bag_to_set(input_list):
 def convert_to_unicode(input_string):
     """Convert input string to unicode."""
     if isinstance(input_string, bytes):
-        return input_string.decode('utf-8')
+        return input_string.decode('utf-8', 'ignore')
     return input_string 
 
 def remove_non_ascii_chars(input_string):
