1daf12b0ce6e1367185970cffe1d4767586954b5
srujithpoondla <spoondla@wisc.edu>
2017-06-12 17:46:33 -0500

    Modified unit test cases: rounded off to 5 decimal places & modified the get_raw_score function in Generalised Jaccard and Monge Elkan measures

--- benchmarks/bm_select_measure.py
+++ benchmarks/bm_select_measure.py
@@ -27,9 +27,9 @@ from py_stringmatching.similarity_measure.monge_elkan import MongeElkan
 #phonetic similarity measures
 from py_stringmatching.similarity_measure.soundex import Soundex
 
-df1 = pd.read_csv('Short_benchmark.csv',encoding='latin')
-df2 = pd.read_csv('Medium_benchmark.csv',encoding='latin')
-df3 = pd.read_csv('Long_benchmark.csv',encoding='latin')
+df1 = pd.read_csv('Short_benchmark.csv', encoding='latin')
+df2 = pd.read_csv('Medium_benchmark.csv', encoding='latin')
+df3 = pd.read_csv('Long_benchmark.csv', encoding='latin')
 
 short_setA = df1['SET A']
 short_setB = df1['SET B']
@@ -37,6 +37,8 @@ medium_setA = df2['SET A']
 medium_setB = df2['SET B']
 long_setA = df3['SET A']
 long_setB = df3['SET B']
+
+
 
 
 measures = {
@@ -66,49 +68,48 @@ measures = {
 
 }
 
-bm_size = [10,20,40,80,160]
+bm_size = [10, 20, 40, 80, 160]
 # bm_size = [10]
-new_index = ['short_short', 'short_medium','short_long', 'medium_medium', 'medium_long', 'long_long' ]
+
+new_index = ['short_short', 'short_medium', 'short_long', 'medium_medium', 'medium_long', 'long_long']
 writer = pd.ExcelWriter('benchmark.xlsx')
-if(os.path.isfile('benchmark.xlsx')):
+if os.path.isfile('benchmark.xlsx'):
     book = load_workbook('benchmark.xlsx')
     writer.book = book
 
 for measure in measures.items():
     # print(str(measure[0]))
     df = pd.DataFrame()
-
-
     for size in bm_size:
 
-        l = []
-        x=bm.benchmark()
+        bm_list = []
+        x = bm.benchmark()
         x.set_bm_pairs(size)
         # l.append(x.get_benchmark(Jaro(), short_setA, short_setB)[0])
         # Benchmark for short-short
-        l.append(x.get_benchmark(measure[1](),short_setA,short_setB)[0])
+        bm_list.append(x.get_benchmark(measure[1](), short_setA, short_setB)[0])
 
         # Benchmark for short-medium
-        l.append(x.get_benchmark(measure[1](),short_setA, medium_setA)[0])
+        bm_list.append(x.get_benchmark(measure[1](), short_setA, medium_setA)[0])
 
         # Benchmark for short-long
-        l.append(x.get_benchmark(measure[1](),short_setA, long_setA)[0])
+        bm_list.append(x.get_benchmark(measure[1](), short_setA, long_setA)[0])
 
         # Benchmark for medium-medium
-        l.append(x.get_benchmark(measure[1](),medium_setA, medium_setB)[0])
+        bm_list.append(x.get_benchmark(measure[1](), medium_setA, medium_setB)[0])
 
         # Benchmark for medium-long
-        l.append(x.get_benchmark(measure[1](),medium_setA, long_setA)[0])
+        bm_list.append(x.get_benchmark(measure[1](), medium_setA, long_setA)[0])
 
         # Benchmark for long-long
-        l.append(x.get_benchmark(measure[1](),long_setA, long_setB)[0])
+        bm_list.append(x.get_benchmark(measure[1](), long_setA, long_setB)[0])
 
-        temp_df = pd.DataFrame({str(size):l},index=new_index)
+        temp_df = pd.DataFrame({str(size): bm_list}, index=new_index)
         # print(temp_df)
         df = pd.concat([df, temp_df], axis=1)
         # print(df)
 
     # print(df)
-    df.to_excel(writer,str(measure[0])+'.xlsx')
+    df.to_excel(writer, str(measure[0])+'.xlsx')
     writer.save()
     #
