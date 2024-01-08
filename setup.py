e10a22572bb3aaf34759a1fb73ce57690cfad6ec
Zachary Ware <zachary.ware@gmail.com>
2024-01-08 12:38:30 -0600

    Drop explicit support for EOL Python 3.7

--- setup.py
+++ setup.py
@@ -118,7 +118,6 @@ if __name__ == "__main__":
             'Operating System :: Microsoft :: Windows',
             'Programming Language :: Python',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10',
